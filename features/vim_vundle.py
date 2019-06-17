a618987d75f192a65faa353277eecbb298a2f268
Moses Palmér <mosespalmer@gmail.com>
2019-06-17 15:33:41 +0200

    Added vim vundle

---
+++ features/vim_vundle.py
@@ -0,0 +1,26 @@
+import os
+
+from . import Feature, feature, git, vim
+
+
+#: The Vundle source repository.
+SOURCE_REPO = 'https://github.com/VundleVim/Vundle.vim'
+
+#: The Vundle installation path.
+TARGET_DIRECTORY = os.path.expanduser('~/.vim/bundle/Vundle.vim')
+
+
+@feature('vim plugin installer', {'git', 'vim'})
+def vim_vundle(env: Feature):
+    git.clone(env, SOURCE_REPO, TARGET_DIRECTORY)
+
+
+@vim_vundle.checker
+def is_installed(env: Feature) -> bool:
+    return os.path.isdir(TARGET_DIRECTORY)
+
+
+@vim_vundle.completer
+def install_plugins(env: Feature):
+    if env.present:
+        vim.run(env, '+PluginInstall', '+qall')
