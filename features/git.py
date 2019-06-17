b16ba4a4fda0acf4e0cce02e109ff7f87182fb15
Moses Palmér <mosespalmer@gmail.com>
2019-06-17 15:26:13 +0200

    Added basic git configuration

---
+++ features/git.py
@@ -0,0 +1,20 @@
+from . import Feature, system
+
+
+system.package(__name__.rsplit('.')[-1])
+
+
+def clone(env: Feature, repository: str, target: str):
+    """Clones a git repository to a specific taget.
+
+    :param env: The feature environment.
+
+    :param repository: The path of the repository.
+
+    :param target: The taget directory. If this is an existing directory, the
+        cloned repo will pick its final name from ``repository``.
+    """
+    env.run(
+        'git', 'clone', '${repository}', '${target}',
+        repository=repository,
+        target=target)
