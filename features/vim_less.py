1d98c01930afd672d61fcb00877730e38af5878d
Moses Palmér <mosespalmer@gmail.com>
2019-09-06 19:51:54 +0200

    Added ron file type plugin for vim

--- features/vim_less.py
+++ features/vim_less.py
@@ -4,15 +4,15 @@ from . import Feature, feature, system, vim
 
 
 BIN = '__vim-less'
+TARGET = os.path.join(os.path.expanduser('~/'), '.local', 'bin', BIN)
 
 
 @feature('vim less.sh', {'vim'})
 def vim_less(env: Feature):
     source = os.path.join(vim.runtime(env), 'macros', 'less.sh')
-    target = os.path.join(os.path.expanduser('~/'), '.local', 'bin', BIN)
-    os.symlink(source, target)
+    os.symlink(source, TARGET)
 
 
 @vim_less.checker
 def is_installed(env: Feature):
-    return system.present(env, BIN)
+    return os.path.islink(TARGET)
