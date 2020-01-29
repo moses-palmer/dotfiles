2cb2d296ee5a4e60f806e1010bdce1b17e101fd3
Moses Palmér <mosespalmer@gmail.com>
2020-01-29 09:21:05 +0100

    Silence Termux detection

--- src/dotfiles/platforms/termux.py
+++ src/dotfiles/platforms/termux.py
@@ -4,5 +4,8 @@ from . import Distribution, Version
 
 
 def information():
-    subprocess.check_call(['which', 'termux-info'])
+    subprocess.check_call(
+        ['which', 'termux-info'],
+        stderr=subprocess.DEVNULL,
+        stdout=subprocess.DEVNULL)
     return (Distribution('Termux', 'termux'), Version('1.0'))
