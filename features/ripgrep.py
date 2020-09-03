6dbf63ecfb54995440cad3836ccc37dd24cbed80
Moses Palmér <mosespalmer@gmail.com>
2020-09-03 09:57:39 +0000

    Fall back on system ripgrep if Rust is unavailable

--- features/ripgrep.py
+++ features/ripgrep.py
@@ -1,4 +1,7 @@
-from . import rust
+from . import FEATURES, rust, system
 
 
-main = rust.binary('rg', 'ripgrep', 'ripgrep')
+if any(f.name == 'rust' for f in FEATURES if not f.blacklisted):
+    main = rust.binary('rg', 'ripgrep', 'ripgrep')
+else:
+    main = system.package('rg', 'ripgrep')
